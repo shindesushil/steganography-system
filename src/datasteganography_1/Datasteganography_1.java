72f70a15b87d61c5529e60fb3f928d63d2efbf9c
Sushil Shinde <sushilshinde9158@gmail.com>
2023-08-16 16:56:04 +0530

    initial commit

---
+++ src/datasteganography_1/Datasteganography_1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datasteganography_1;
+
+
+public class Datasteganography_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+         OperationFrame01 home = new OperationFrame01();
+            home.setVisible(true);
+    }
+    
+}
